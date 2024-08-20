fd0a605159640b83f512ba110acaea50a4cd6349
Mark Sharp <mark.sharp@solent.ac.uk>
2024-08-20 15:30:44 +0100

    Updated theme for 24.04

--- themeconfig.php
+++ themeconfig.php
@@ -3,8 +3,8 @@
  *
  * @package    mahara
  * @subpackage core
- * @author     Catalyst IT Ltd
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @author     Catalyst IT Limited <mahara@catalyst.net.nz>
+ * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  */
 
@@ -14,10 +14,19 @@ $theme = new stdClass();
 $theme->displayname = 'Solent';
 
 /* Set parent to boolean FALSE to specify the theme has no parent */
+/* If your parent theme is a subtheme, for example Modern, Default, Ocean, or Primaryschool,
+you will need to uncomment the import lines with the path "../../<your parent theme name>/sass
+in sass/style.scss and change the value <your parent theme name> to the folder name of the
+subtheme that you use as basis. */
 $theme->parent = 'raw';
 
 /* If we are using normal CSS, this should be false. If we are using SASS, it should be true. */
 $theme->overrideparentcss = true;
+
+$theme->themeaddressbar = '#ffffff';
+
+/* TRUE if your theme includes dark mode, FALSE if you don't want dark mode to be allowed */
+$theme->allowdarkmode = false;
 
 /**
  * The following themeconfig options are available. If you make new themeconfig
